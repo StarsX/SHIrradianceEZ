fac70fd726e6c687be6b9e54b181ec15947daae7
StarsX <X-Universe@outlook.com>
2023-05-03 16:45:37 +0800

    XUSG fix of root signature highest version for vkd3d-proton

--- SHIrradianceEZ/SHIrradianceEZ.h
+++ SHIrradianceEZ/SHIrradianceEZ.h
@@ -76,10 +76,6 @@ private:
 	XMFLOAT3	m_focusPt;
 	XMFLOAT3	m_eyePt;
 
-	// Screen-shot helper
-	XUSG::Buffer::uptr	m_readBuffer;
-	uint32_t			m_rowPitch;
-
 	// Synchronization objects.
 	uint8_t		m_frameIndex;
 	HANDLE		m_fenceEvent;
@@ -102,8 +98,10 @@ private:
 	std::vector<std::wstring> m_envFileNames;
 	XMFLOAT4 m_meshPosScale;
 
-	// Screen-shot state
-	uint8_t m_screenShot;
+	// Screen-shot helpers and state
+	XUSG::Buffer::uptr	m_readBuffer;
+	uint32_t			m_rowPitch;
+	uint8_t				m_screenShot;
 
 	void LoadPipeline();
 	void LoadAssets();
